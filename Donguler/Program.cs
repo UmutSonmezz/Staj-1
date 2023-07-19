31348eece8e38a8028402e46b480e5c95b3110a7
umut <umut@DESKTOP-10UJKPB>
2023-07-19 10:45:51 +0300

    Proje dosyası ekle.

---
+++ Donguler/Program.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yerleştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yerleştirme Kampı", "Programlamaya başlangıç için temel kurs", "Java","Python","C#"}; 
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs  in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ Metotlar/Sepet Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Sepet_Manager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi:  " + urun.Adi );
+        }
+
+        public void  Ekle2()
+    }
+}
